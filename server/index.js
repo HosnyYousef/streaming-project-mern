7e9001dc187aee52d0647a71638dde60299439ca
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2025-08-31 10:07:54 -0700

    added code

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRoutes from "./routes/users.js"
 
 const app = express()
 dotenv.config()
@@ -12,8 +13,9 @@ const connect = () =>{
     .catch((err) => {
         throw err;
     });
-}
+};
 
+app.use("/api/users", userRoutes)
 
 app.listen(8800, ()=>{
     connect()
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { test } from "../controllers/user.js";
 
 const router = express.Router()
 
-router.get("/test", )
+router.get("/test", test)
 // because we are going to fetch data
 
 export default router;
