973aa9e8a1f560b42c36094c3fe70462016b592c
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2025-09-07 13:46:37 -0700

    code added

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -56,8 +56,8 @@ export const getVideo = async (req, res, next) => {
 
 export const addView = async (req, res, next) => {
     try {
-        const video = await Video.findById(req.params.id)
-        res.status(200).json(video)
+        const video = await Video.findByIdAndUpdate(req.params.id, { $inc: { views: 1 } })
+        res.status(200).json("The view has been increased")
     } catch (err) {
         next(err)
     }
@@ -65,8 +65,8 @@ export const addView = async (req, res, next) => {
 
 export const random = async (req, res, next) => {
     try {
-        const video = await Video.findById(req.params.id)
-        res.status(200).json(video)
+        const videos = await Video.aggregate([{ $sample: { size: 40 } }])
+        res.status(200).json(videos)
     } catch (err) {
         next(err)
     }
@@ -74,8 +74,8 @@ export const random = async (req, res, next) => {
 
 export const trend = async (req, res, next) => {
     try {
-        const video = await Video.findById(req.params.id)
-        res.status(200).json(video)
+        const videos = await Video.find().sort({views:-1})
+        res.status(200).json(videos)
     } catch (err) {
         next(err)
     }
