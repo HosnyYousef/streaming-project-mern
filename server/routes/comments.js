c196225772c6dbe7ebe1511ac7380e44299ebc42
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2025-09-10 11:44:25 -0700

    code added

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { addComment, deleteComment, getComment } from "../controllers/comment.js";
+import { addComment, deleteComment, getComments } from "../controllers/comment.js";
 import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router()
 
 router.post("/", verifyToken, addComment)
 router.post("/:id", verifyToken, deleteComment)
-router.post("/:videoId", verifyToken, getComment)
+router.post("/:videoId", verifyToken, getComments)
 
 export default router;
