24c3148bddbb6e63a906de6f2e45a95a317ecb70
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2025-09-10 18:08:30 -0700

    code added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -73,16 +73,28 @@ export const unsubscribe = async (req, res, next) => {
 }
 
 export const like = async (req, res, next) => {
+    const id = req.user.id;
+    const videoId = req.user.videoId
     try {
-
+        await Video.findByIdAndUpdate(videoId, {
+            $addToSet: { likes: id },
+            $pull: { dislikes: id }
+        })
+        res.status(200).json("The video has been liked.")
     } catch (err) {
         next(err)
     }
 }
 
 export const dislike = async (req, res, next) => {
+    const id = req.user.id;
+    const videoId = req.params.videoId
     try {
-
+        await Video.findByIdAndUpdate(videoId, {
+            $addToSet: { dislikes: id },
+            $pull: { likes: id }
+        })
+        res.status(200).json("The video has been liked.")
     } catch (err) {
         next(err)
     }
