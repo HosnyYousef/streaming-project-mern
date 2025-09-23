8afa7ba1d5ab2b27afb2ccae875cff8b6e46e9a7
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2025-09-23 10:11:38 -0700

    wrapped up trending video, ordering number. But does not order after refreshing a few times

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { format } from "timeago.js";
 
 const Container = styled.div`
   width: ${(props) => props.type !== "sm" && "360px"};
@@ -52,6 +54,15 @@ const Info = styled.div`
 `;
 
 const Card = ({ type, video }) => {
+  const [channel, setChannel] = useState({})
+
+  useEffect(() => {
+    const fetchChannel = async () => {
+      const res = await axios.get(`/users/find/${video.userId}`)
+      setChannel(res.data)
+    }
+    fetchChannel()
+  }, [video.userId])
   return (
     <Link to="/video/test" style={{ textDecoration: "none" }}>
       <Container type={type}>
@@ -62,12 +73,12 @@ const Card = ({ type, video }) => {
         <Details type={type}>
           <ChannelImage
             type={type}
-            src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo"
+            src= {channel.img}
           />
           <Texts>
             <Title>{video.title}</Title>
-            <ChannelName>Troopa Dev</ChannelName>
-            <Info>{video.views} views • {video.createdAt}</Info>
+            <ChannelName>{channel.name}</ChannelName>
+            <Info>{video.views} views • {format(video.createdAt)}</Info>
           </Texts>
         </Details>
       </Container>
