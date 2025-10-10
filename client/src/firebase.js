3645fb91090db63810e6c86f00337a885dd62fdb
Yousef Hosny <94009699+HosnyYousef@users.noreply.github.com>
2025-10-10 17:02:38 +0300

    firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: fireBaseAPIKey,
+  authDomain: "video-f66b8.firebaseapp.com",
+  projectId: "video-f66b8",
+  storageBucket: "video-f66b8.firebasestorage.app",
+  messagingSenderId: "532960581145",
+  appId: "1:532960581145:web:a9dba452266900b28afc88"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
+export const provider = new GoogleAuthProvider()
+
+export default app
